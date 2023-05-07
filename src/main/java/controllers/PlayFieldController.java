a01634da62df6be959e5a6a8b506b50d69c0d784
danil <danilromanov999@gmail.com>
2023-05-07 11:21:02 +0700

    Top collision is done

--- src/main/java/controllers/PlayFieldController.java
+++ src/main/java/controllers/PlayFieldController.java
@@ -7,40 +7,19 @@ import javafx.fxml.Initializable;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
-import javafx.scene.shape.Rectangle;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.ResourceBundle;
 
-public class PlayFieldController implements Initializable {
+public class PlayFieldController extends PlayFieldView implements Initializable {
     @FXML
-    private Pane pacmanPane;
+    protected Pane pacmanPane;
     @FXML
-    private ImageView pacmanImage;
-    @FXML
-    private HBox wall1;
-    @FXML
-    private HBox wall2;
-    @FXML
-    private HBox wall3;
-    @FXML
-    private HBox wall4;
-    @FXML
-    private HBox wall5;
-    @FXML
-    private Rectangle wall6;
-    @FXML
-    private Pane corner1;
-    @FXML
-    private Pane corner2;
-    @FXML
-    private Pane corner3;
+    protected ImageView pacmanImage;
     private Direction direction;
     private Direction potentialDirection;
+    private double imageRotation;
 
     public void setPotentialDirection(Direction potentialDirection) {
         this.potentialDirection = potentialDirection;
@@ -56,57 +35,48 @@ public class PlayFieldController implements Initializable {
     @FXML
     private void keyDispatcher(KeyEvent keyEvent) {
         if (keyEvent.getCode() == KeyCode.A) {
-            pacmanImage.setRotate(180);
+            imageRotation = 180;
             setPotentialDirection(Direction.LEFT);
         }
         else if (keyEvent.getCode() == KeyCode.W) {
-            pacmanImage.setRotate(270);
+            imageRotation = 270;
             setPotentialDirection(Direction.UP);
         }
         else if (keyEvent.getCode() == KeyCode.D) {
-            pacmanImage.setRotate(0);
+            imageRotation = 0;
             setPotentialDirection(Direction.RIGHT);
         }
         else if (keyEvent.getCode() == KeyCode.S) {
-            pacmanImage.setRotate(90);
+            imageRotation = 90;
             setPotentialDirection(Direction.DOWN);
         }
     }
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        //setPotentialDirection(Direction.LEFT);
+        super.initialize();
         setDirection(Direction.LEFT);
         setPotentialDirection(Direction.LEFT);
+
         Runnable pacmanMove = new Runnable() {
             @Override
             public void run() {
-                List<Pane> corners = new ArrayList<>();
-                corners.add(corner1);
-                corners.add(corner2);
-                corners.add(corner3);
-
                 AnimationTimer timer = new AnimationTimer() {
                     @Override
                     public void handle(long l) {
                         System.out.println(direction + " " + potentialDirection);
-                        long interseptions = corners.stream().filter(bounds -> bounds.getBoundsInParent().intersects(pacmanPane.getBoundsInParent())).count();
-                        if (interseptions > 0) {
-                            if (potentialDirection != direction) {
-                                setDirection(potentialDirection);
-                            }
-                            else {
-                                setDirection(Direction.NONE);
-                                setPotentialDirection(Direction.NONE);
-                            }
+                        long cornerIntersections = corners.stream().filter(bounds -> bounds.getBoundsInParent().intersects(pacmanPane.getBoundsInParent())).count();
+                        long wallIntersections = walls.stream().filter(bounds -> bounds.getBoundsInParent().intersects(pacmanImage.getBoundsInParent())).count();
+                        if (cornerIntersections > 0) {
+                            setDirection(potentialDirection);
                         }
-                        /*if (interseptions < 0) { // pacmanPane.getBoundsInParent().intersects(leftWall.getBoundsInParent()) ||
+                        else if (wallIntersections > 0) {
                             if (direction == Direction.LEFT) {  // pacmanPane.getBoundsInParent().intersects(topWall.getBoundsInParent())
                                 pacmanPane.setLayoutX(pacmanPane.getLayoutX() + 2);
                                 pacmanImage.setLayoutX(pacmanImage.getLayoutX() + 2);
                             }
                             else if (direction == Direction.UP) {
                                 pacmanPane.setLayoutY(pacmanPane.getLayoutY() + 2);
-                                pacmanImage.setLayoutY(pacmanImage.getLayoutX() + 2);
+                                pacmanImage.setLayoutY(pacmanImage.getLayoutY() + 2);
                             }
                             else if (direction == Direction.RIGHT) {
                                 pacmanPane.setLayoutX(pacmanPane.getLayoutX() - 2);
@@ -114,10 +84,11 @@ public class PlayFieldController implements Initializable {
                             }
                             else if (direction == Direction.DOWN) {
                                 pacmanPane.setLayoutY(pacmanPane.getLayoutY() - 2);
-                                pacmanImage.setLayoutY(pacmanImage.getLayoutX() - 2);
+                                pacmanImage.setLayoutY(pacmanImage.getLayoutY() - 2);
                             }
                             setPotentialDirection(Direction.NONE);
-                        }*/
+                            setDirection(potentialDirection);
+                        }
                         if (direction == Direction.LEFT) {
                             pacmanPane.setLayoutX(pacmanPane.getLayoutX() - 1);
                             pacmanImage.setLayoutX(pacmanImage.getLayoutX() - 1);
---
+++ src/main/java/domain/Crossroad.java
@@ -0,0 +1,9 @@
+package domain;
+
+import javafx.scene.layout.Pane;
+
+public class Crossroad extends Pane {
+    public Crossroad() {
+
+    }
+}
